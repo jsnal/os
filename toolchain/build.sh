a0b8c905e25024fc591e16ce85da7931d8abaade
jsnal <jasonlongball@gmail.com>
2021-12-12 15:19:07 -0500

    [Toolchain] Build toolchain for custom target
    This will be helpful when creating my own C library later on. It's also
    just a nice thing to have early on I think.

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -6,10 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ARCH="i686"
 PREFIX="$DIR/local"
-TARGET="$ARCH-elf"
+TARGET="$ARCH-pc-os"
 PATH="$PREFIX/bin:$PATH"
 
-BINUTILS_VERSION="2.37"
+BINUTILS_VERSION="2.32"
 BINUTILS_NAME="binutils-$BINUTILS_VERSION"
 BINUTILS_PKG="${BINUTILS_NAME}.tar.gz"
 BINUTILS_BASE_URL="https://ftp.gnu.org/gnu/binutils"
@@ -26,11 +26,17 @@ if [ ! -d ${BINUTILS_NAME} ]; then
     curl -LO "$BINUTILS_BASE_URL/$BINUTILS_PKG"
 
     echo "Extrating binutils"
-    # if [ -d ${BINUTILS_NAME} ]; then
-    #   rm -rf "${BINUTILS_NAME}"
-    #   rm -rf "$DIR/local/$BINUTILS_NAME"
-    # fi
     tar -xzf ${BINUTILS_PKG}
+
+    echo "Patching binutils"
+    pushd $BINUTILS_NAME
+      patch -p1 < ../../binutils-2.32.patch
+      pushd ld
+        automake
+      popd
+    popd
+
+    echo "Building binutils"
     ${BINUTILS_NAME}/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
     make -j
     make install
@@ -41,11 +47,19 @@ if [ ! -d ${GCC_NAME} ]; then
   mkdir $GCC_NAME
   pushd $GCC_NAME
     which -- $TARGET-as || echo $TARGET-as is not in the PATH
+
     echo "Downloading gcc"
     curl -LO "$GCC_BASE_URL/$GCC_NAME/$GCC_PKG"
 
     echo "Extrating gcc"
     tar -xzf ${GCC_PKG}
+
+    echo "Patching gcc"
+    pushd $GCC_NAME
+      patch -p1 < ../../gcc-10.2.0.patch
+    popd
+
+    echo "Building gcc"
     ${GCC_NAME}/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers
     make all-gcc
     make all-target-libgcc
