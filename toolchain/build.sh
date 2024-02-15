2ca39fdd4b1c201c01691f552920cbdab8b82242
Jason Long <jasonlongball@gmail.com>
2024-02-14 20:30:52 -0500

    toolchain: use git submodules for binutils and gcc

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -9,65 +9,65 @@ PREFIX="$DIR/local"
 TARGET="$ARCH-pc-os"
 PATH="$PREFIX/bin:$PATH"
 
-BINUTILS_VERSION="2.32"
-BINUTILS_NAME="binutils-$BINUTILS_VERSION"
-BINUTILS_PKG="${BINUTILS_NAME}.tar.gz"
-BINUTILS_BASE_URL="https://ftp.gnu.org/gnu/binutils"
+if [ ! -d "binutils" ] || [ ! -d "gcc" ]; then
+    echo "Submodules have not been cloned"
+fi
 
-GCC_VERSION="10.2.0"
-GCC_NAME="gcc-$GCC_VERSION"
-GCC_PKG="${GCC_NAME}.tar.gz"
-GCC_BASE_URL="https://ftp.gnu.org/gnu/gcc"
-
-if [ ! -d ${BINUTILS_NAME} ]; then
-  mkdir $BINUTILS_NAME
-  pushd $BINUTILS_NAME
-    echo "Downloading binutils"
-    curl -LO "$BINUTILS_BASE_URL/$BINUTILS_PKG"
-
-    echo "Extrating binutils"
-    tar -xzf ${BINUTILS_PKG}
-
-    echo "Patching binutils"
-    pushd $BINUTILS_NAME
-      patch -p1 < ../../binutils-2.32.patch
-      pushd ld
+pushd "binutils"
+    echo "Automaking LD"
+    pushd ld
         autoreconf
         automake
-      popd
     popd
 
-    echo "Building binutils"
-    ${BINUTILS_NAME}/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-    make all -j4
-    make install
+    if [ ! -d "build" ]; then
+        mkdir build
+        pushd "build"
+        echo "Configuring binutils"
+        ../configure \
+            --target=$TARGET \
+            --prefix="$PREFIX" \
+            --with-sysroot \
+            --disable-nls \
+            --disable-werror
+
+        echo "Building binutils"
+        make all -j4
+
+        echo "Installing binutils"
+        make install
+        popd
+    fi
+
+popd
+
+pushd "gcc"
+  echo "Downloading dependencies"
+  ./contrib/download_prerequisites
+
+  echo "Automaking libstd++-v3"
+  pushd libstdc++-v3
+    autoconf
   popd
-fi
 
-if [ ! -d ${GCC_NAME} ]; then
-  mkdir $GCC_NAME
-  pushd $GCC_NAME
-    which -- $TARGET-as || echo $TARGET-as is not in the PATH
+  if [ ! -d "build" ]; then
+      mkdir build
+  fi
 
-    echo "Downloading gcc"
-    curl -LO "$GCC_BASE_URL/$GCC_NAME/$GCC_PKG"
+  pushd "build"
+      echo "Configuring gcc"
+      ../configure --target=$TARGET \
+          --prefix="$PREFIX" \
+          --disable-nls \
+          --without-headers \
+          --enable-languages=c,c++
 
-    echo "Extrating gcc"
-    tar -xzf ${GCC_PKG}
+      echo "Building gcc"
+      make all-gcc -j4
+      make all-target-libgcc -j4
 
-    echo "Patching gcc"
-    pushd $GCC_NAME
-      patch -p1 < ../../gcc-10.2.0.patch
-
-      echo "Downloading dependencies"
-      ./contrib/download_prerequisites
-    popd
-
-    echo "Building gcc"
-    ${GCC_NAME}/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers
-    make all-gcc -j4
-    make all-target-libgcc -j4
-    make install-gcc -j4
-    make install-target-libgcc -j4
+      echo "Installing gcc"
+      make install-gcc -j4
+      make install-target-libgcc -j4
   popd
-fi
+popd
