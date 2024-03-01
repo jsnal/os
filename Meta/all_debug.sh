74417eee67a573d649a694b0112947394a794fe9
Jason Long <jasonlongball@gmail.com>
2024-02-29 20:17:24 -0500

    Meta: Add all_debug script for starting GDB sesssion

---
+++ Meta/all_debug.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+command -v tmux >/dev/null 2>&1 || (echo "tmux not installed" && exit 1)
+
+. ./Meta/env.sh && \
+    make -C Build install && \
+    ./Meta/build_grub.sh && \
+
+GDB_COMMAND="gdb Kernel/Kernel \
+   -ex 'set arch i386:intel' \
+   -ex 'layout asm' \
+   -ex 'layout regs' \
+   -ex 'target remote localhost:1234' \
+   -ex 'fs next'"
+
+tmux \
+    new-session  './Meta/run.sh gdb' \; \
+    set-option -t 0 mouse on \; \
+    split-window "$GDB_COMMAND" \; \
