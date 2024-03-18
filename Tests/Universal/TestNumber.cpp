280627f290beb355cfb5b80a0503f30ea76f1c96
Jason Long <jasonlongball@gmail.com>
2024-03-18 01:00:49 -0400

    Tests: Add tests for Number class

--- Tests/Universal/TestNumber.cpp
+++ Tests/Universal/TestNumber.cpp
@@ -7,11 +7,30 @@
 #include <Tests/Macros.h>
 #include <Universal/Number.h>
 
+TEST_CASE(number_between)
+{
+    CHECK_TRUE(number_between(2, 1, 3));
+    CHECK_TRUE(number_between(1.5, 1, 2));
+    CHECK_FALSE(number_between(0, 1, 3));
+    CHECK_FALSE(number_between(1, 1, 3));
+}
+
+TEST_CASE(number_between_inclusive)
+{
+    CHECK_TRUE(number_between_inclusive(2, 1, 3));
+    CHECK_TRUE(number_between_inclusive(1.5, 1, 2));
+    CHECK_FALSE(number_between_inclusive(0, 1, 3));
+    CHECK_TRUE(number_between_inclusive(1, 1, 3));
+}
+
 TEST_CASE(ceiling_divide)
 {
-    CHECK_FALSE(false);
+    CHECK_EQUAL(1, ceiling_divide(1, 2));
+    CHECK_EQUAL(2, ceiling_divide(3, 2));
 }
 
 TEST_MAIN(TestNumber, [&]() {
     ENUMERATE_TEST(ceiling_divide);
+    ENUMERATE_TEST(number_between_inclusive);
+    ENUMERATE_TEST(number_between);
 })
