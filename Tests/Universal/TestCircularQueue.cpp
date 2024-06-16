3b2fcd8383011af3de8c980028b7742548d1679e
Jason Long <jasonlongball@gmail.com>
2024-06-16 19:53:27 -0400

    Universal: Add CircularQueue data structure

---
+++ Tests/Universal/TestCircularQueue.cpp
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2024, Jason Long <jasonlongball@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <Tests/Macros.h>
+#include <Universal/CircularQueue.h>
+
+TEST_CASE(create)
+{
+    auto queue = CircularQueue<u32>(10);
+    CHECK_EQUAL((size_t)0, queue.size());
+    CHECK_EQUAL((size_t)10, queue.capacity());
+}
+
+TEST_CASE(enqueue_dequeue)
+{
+    auto queue = CircularQueue<u32>(4);
+    CHECK_EQUAL((size_t)0, queue.size());
+    CHECK_EQUAL((size_t)4, queue.capacity());
+
+    CHECK_TRUE(queue.enqueue(1).is_ok());
+    CHECK_TRUE(queue.enqueue(2).is_ok());
+    CHECK_TRUE(queue.enqueue(3).is_ok());
+    CHECK_TRUE(queue.enqueue(4).is_ok());
+    CHECK_FALSE(queue.enqueue(5).is_ok());
+
+    CHECK_EQUAL((u32)1, queue.dequeue().value());
+    CHECK_EQUAL((u32)2, queue.dequeue().value());
+    CHECK_EQUAL((u32)3, queue.dequeue().value());
+    CHECK_EQUAL((u32)4, queue.dequeue().value());
+    CHECK_TRUE(queue.dequeue().is_error());
+
+    CHECK_TRUE(queue.enqueue(1).is_ok());
+    CHECK_TRUE(queue.enqueue(2).is_ok());
+    CHECK_TRUE(queue.enqueue(3).is_ok());
+
+    CHECK_EQUAL((u32)1, queue.dequeue().value());
+    CHECK_EQUAL((u32)2, queue.dequeue().value());
+
+    CHECK_TRUE(queue.enqueue(4).is_ok());
+
+    CHECK_EQUAL((u32)3, queue.dequeue().value());
+    CHECK_EQUAL((u32)4, queue.dequeue().value());
+}
+
+TEST_CASE(front_back)
+{
+    auto queue = CircularQueue<u32>(4);
+    CHECK_EQUAL((size_t)0, queue.size());
+    CHECK_EQUAL((size_t)4, queue.capacity());
+
+    CHECK_TRUE(queue.enqueue(1).is_ok());
+    CHECK_TRUE(queue.enqueue(2).is_ok());
+    CHECK_TRUE(queue.enqueue(3).is_ok());
+    CHECK_TRUE(queue.enqueue(4).is_ok());
+
+    CHECK_EQUAL((u32)1, queue.front());
+    CHECK_EQUAL((u32)4, queue.back());
+
+    CHECK_EQUAL((u32)1, queue.dequeue().value());
+    CHECK_EQUAL((u32)2, queue.front());
+    CHECK_EQUAL((u32)4, queue.back());
+
+    CHECK_EQUAL((u32)2, queue.dequeue().value());
+    CHECK_EQUAL((u32)3, queue.front());
+    CHECK_EQUAL((u32)4, queue.back());
+
+    CHECK_EQUAL((u32)3, queue.dequeue().value());
+    CHECK_EQUAL((u32)4, queue.front());
+    CHECK_EQUAL((u32)4, queue.back());
+
+    CHECK_EQUAL((u32)4, queue.dequeue().value());
+    CHECK_TRUE(queue.empty());
+}
+
+TEST_CASE(move)
+{
+    auto queue = CircularQueue<u32>(4);
+    CHECK_EQUAL((size_t)0, queue.size());
+    CHECK_EQUAL((size_t)4, queue.capacity());
+
+    CHECK_TRUE(queue.enqueue(1).is_ok());
+    CHECK_TRUE(queue.enqueue(2).is_ok());
+    CHECK_TRUE(queue.enqueue(3).is_ok());
+    CHECK_TRUE(queue.enqueue(4).is_ok());
+
+    auto queue_construct = CircularQueue<u32>(move(queue));
+    CHECK_FALSE(queue_construct.empty());
+    CHECK_TRUE(queue.empty());
+    CHECK_EQUAL((size_t)4, queue_construct.size());
+    CHECK_EQUAL((size_t)4, queue_construct.capacity());
+
+    CHECK_EQUAL((u32)1, queue_construct.front());
+    CHECK_EQUAL((u32)4, queue_construct.back());
+
+    auto queue_equals = CircularQueue<u32>(queue_construct.size());
+    CHECK_TRUE(queue_equals.empty());
+    CHECK_EQUAL((size_t)4, queue_equals.capacity());
+
+    queue_equals = move(queue_construct);
+
+    CHECK_TRUE(queue.empty());
+    CHECK_TRUE(queue_construct.empty());
+    CHECK_EQUAL((u32)1, queue_equals.front());
+    CHECK_EQUAL((u32)4, queue_equals.back());
+}
+
+TEST_MAIN(TestCircularQueue, [&]() {
+    ENUMERATE_TEST(create);
+    ENUMERATE_TEST(enqueue_dequeue);
+    ENUMERATE_TEST(front_back);
+    ENUMERATE_TEST(move);
+})
---
+++ Universal/CircularQueue.h
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2024, Jason Long <jasonlongball@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <Universal/Result.h>
+#include <Universal/Types.h>
+
+template<typename T>
+class CircularQueue {
+public:
+    CircularQueue(size_t capacity)
+        : m_capacity(capacity)
+        , m_size(0)
+    {
+        m_queue = new T[capacity];
+    }
+
+    CircularQueue(CircularQueue&& other)
+        : m_queue(other.m_queue)
+        , m_capacity(other.m_capacity)
+        , m_size(other.m_size)
+        , m_front(other.m_front)
+        , m_back(other.m_back)
+    {
+        other.m_queue = nullptr;
+        other.m_capacity = 0;
+        other.m_size = 0;
+        other.m_front = 0;
+        other.m_back = 0;
+    }
+
+    CircularQueue& operator=(CircularQueue&& other)
+    {
+        if (this != &other) {
+            delete[] m_queue;
+
+            m_queue = other.m_queue;
+            m_capacity = other.m_capacity;
+            m_size = other.m_size;
+            m_front = other.m_front;
+            m_back = other.m_back;
+
+            other.m_queue = nullptr;
+            other.m_capacity = 0;
+            other.m_size = 0;
+            other.m_front = 0;
+            other.m_back = 0;
+        }
+        return *this;
+    }
+
+    ~CircularQueue()
+    {
+        delete[] m_queue;
+    }
+
+    size_t capacity() const { return m_capacity; }
+    size_t size() const { return m_size; }
+
+    bool is_full() { return m_size == m_capacity; }
+    bool empty() { return m_size == 0; }
+
+    Result enqueue(const T& element)
+    {
+        if (is_full()) {
+            return Result::Failure;
+        }
+
+        if (empty()) {
+            m_front = 0;
+            m_back = 0;
+        } else {
+            m_back = (m_back + 1) % m_capacity;
+        }
+
+        m_queue[m_back] = element;
+        m_size++;
+
+        return Result::OK;
+    }
+
+    ResultReturn<T> dequeue()
+    {
+        if (empty()) {
+            return Result(Result::Failure);
+        }
+
+        T element = m_queue[m_front];
+        m_size--;
+
+        if (empty()) {
+            m_front = 0;
+            m_back = 0;
+        } else {
+            m_front = (m_front + 1) % m_capacity;
+        }
+
+        return element;
+    }
+
+    T& front() const { return m_queue[m_front]; }
+    T& back() const { return m_queue[m_back]; }
+
+private:
+    T* m_queue { nullptr };
+    size_t m_capacity { 0 };
+    size_t m_size { 0 };
+    size_t m_front { 0 };
+    size_t m_back { 0 };
+};
